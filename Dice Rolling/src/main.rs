eda828711ef989cab8cf2fe648dc2f5ba16ec0eb
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-04-02 01:18:04 -0500

    code 20 new problems

---
+++ Dice Rolling/src/main.rs
@@ -0,0 +1,43 @@
+/* MIT License
+ *
+ * Copyright (c) 2021 Brighton Sikarskie
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the Software), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#![allow(unused_imports)]
+use std::{
+    cmp,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let input = BufReader::new(File::open("input")?);
+
+    for line in input.lines().skip(1) {
+        let nums: Vec<f32> = line?
+            .split_whitespace()
+            .map(|i| i.parse().unwrap())
+            .collect();
+        print!("{} ", (nums[0] * 6.) as u32 + 1);
+    }
+
+    Ok(())
+}
