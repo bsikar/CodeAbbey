3a3b84637403ce07b5b406329f0f37d4d7495104
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-04-03 21:42:47 -0500

    code

---
+++ rust/Bubble Sort/src/main.rs
@@ -0,0 +1,56 @@
+/* MIT License
+ *
+ * Copyright (c) 2021 Brighton Sikarskie
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the Software), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#![allow(unused_imports)]
+use std::{
+    cmp,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let input = BufReader::new(File::open("input")?);
+
+    for line in input.lines().skip(1) {
+        let mut nums: Vec<i32> = line?
+            .split_whitespace()
+            .map(|i| i.parse().unwrap())
+            .collect();
+        let mut x: (u32, u32) = (0, 0);
+        let mut sorted = false;
+        while !sorted {
+            sorted = true;
+            for o in 0..nums.len() - 1 {
+                if nums[o] > nums[o + 1] {
+                    nums.swap(o, o + 1);
+                    x.1 += 1;
+                    sorted = false;
+                }
+            }
+            x.0 += 1;
+        }
+        print!("{} {} ", x.0, x.1);
+    }
+
+    Ok(())
+}
